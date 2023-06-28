1330db463c232907b13da0d6837523e1debd0514
PolinaK <polina.kirichencko@yandex.ru>
2023-06-28 16:44:19 +0400

    added test of presence of the link and selenium-method for it with catching the exception

--- pages/PageObject/base_page.py
+++ pages/PageObject/base_page.py
@@ -1,7 +1,17 @@
-class BasePage:  #базовая страница=1 класс, от кот наследуются остальные классы
-    def __init__(self, browser, url): #вспомог методы для работы с драйвером
+from selenium.common.exceptions import NoSuchElementException
+
+class BasePage:  #базовая страница=1 класс, от кот наследуются остальные классы - КЛАСС ДЛЯ НАСТРОЙКИ ОКРУЖЕНИЯ, ПОЛУЧЕНИЯ ССЫЛКИ И ОБРАБОТК ИСКЛЮЧ
+    def __init__(self, browser, url, timeout=10): #вспомог методы для работы с драйвером
         self.browser = browser #!!!экземпляр браузера передаем
         self.url = url  #ссылка
+        self.browser.implicitly_wait(timeout) #команду для неявного ожидания со значением по умолчанию в 10
 
     def open(self):  #открытие стр в браузере
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what): #в котором будем перехватывать исключение. В него будем передавать два аргумента: как искать (css, id, xpath и тд) и собственно что искать (строку-селектор).
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- pages/PageObject/main_page.py
+++ pages/PageObject/main_page.py
@@ -2,7 +2,12 @@ from pages.PageObject.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 
-class MainPage(BasePage): #наследуется от базового класса
+class MainPage(BasePage): #наследуется от базового класса - КЛАСС ДЛЯ КЛИКАНИЙ И МЕЛКИХ ФРОНТЕНД ДЕЙСТВИЙ
+    def should_be_login_link(self): #проверка наличия ссылки на лог-ин(вызов метода из баз класса, чтобы обработать исключение и сделать осмысленный вывод)
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
     def go_to_login_page(self): #переход на логин-стр
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link") #поиск кнопки перехода на логин
         login_link.click() #кликаем
+
+
+#Это хорошая практика: писать сначала красные тесты и только потом делать их зелеными.
