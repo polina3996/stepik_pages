34f0e54ff1f00101e409356c0f071a16803d74e5
PolinaK <polina.kirichencko@yandex.ru>
2023-06-29 16:37:28 +0400

    Added switch between main page and login page

--- pages/test_main_page.py
+++ pages/test_main_page.py
@@ -1,4 +1,6 @@
 from pages.PageObject.main_page import MainPage
+from login_page import LoginPage
+
 #ОСНОВНАЯ ЛОГИКА ТЕСТОВ
 link =  "http://selenium1py.pythonanywhere.com/"
 def test_guest_should_see_login_link(browser):
@@ -9,5 +11,8 @@ def test_guest_should_see_login_link(browser):
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    #login_page = page.go_to_login_page()  # 1 способ:выполняем метод страницы — переходим на страницу логина и сохраняем возвращаемый объект стр в переменную
+    page.go_to_login_page() #2 способ:
+    login_page = LoginPage(browser, browser.current_url) #2 способ:Инициализируем LoginPage в теле теста
+    login_page.should_be_login_page() #1 и 2 способ:проверка, что стр есть
 
